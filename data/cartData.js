4aeab52f7498ea6c248692b12dfecc55601cc9fe
Sangharsha <sangamtyata3@gmail.com>
2025-07-16 18:05:24 +0545

    added carts

--- data/cartData.js
+++ data/cartData.js
@@ -1,3 +1,18 @@
 export const CartData = [
-    {}
-]
+  {
+    _id: 2,
+    name: "Ferrari 499P",
+    brand: "Ferrari",
+    price: 750,
+    stock: 10,
+    image: require("../assets/images/ferrari499p.png"),
+  },
+  {
+    _id: 3,
+    name: "Porsche 911 GT3",
+    brand: "Porsche",
+    price: 1050,
+    stock: 10,
+    image: require("../assets/images/porsche911.png"),
+  },
+];
